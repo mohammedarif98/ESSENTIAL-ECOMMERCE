d9de587ec1f86191bc5b132615961cafb665970d
mohammedarif98 <arifcsidhik@gmail.com>
2024-07-03 09:30:43 +0530

    update

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -6,7 +6,7 @@ const dbConnect = async()=>{
         await mongoose.connect(process.env.MONGODB_URL);
         console.log(`database connected`);
     }catch(error){
-        console.log("database error");
+        console.log("database error",error);
         process.exit(1)  //application exits with a status code of 1, indicating an error.
     }
 } 
