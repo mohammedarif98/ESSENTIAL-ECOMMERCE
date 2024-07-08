e9cf3cfa15c5adb8ddc6dcdf7fa9113a4fb62b9d
mohammedarif98 <arifcsidhik@gmail.com>
2024-07-08 09:30:09 +0530

    google auth updated

--- passport.js
+++ passport.js
@@ -18,7 +18,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID, 
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/auth/google/callback",
+      // callbackURL: "http://localhost:4000/auth/google/callback",
+      callbackURL: "https://essentialstore.live/auth/google/callback",
       passReqToCallback: true, 
     },
     function (request, accessToken, refreshToken, profile, cb) {
